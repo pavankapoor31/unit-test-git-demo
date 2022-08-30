e0c2f50b60139ecf4a3168e2b5d4c53654192d1d
atharsh <atharshsivasamy99@gmail.com>
2022-08-30 12:12:47 +0530

    atharshTestFunction

--- lib.js
+++ lib.js
@@ -3,3 +3,20 @@ module.exports.absolute = function (number) {
     return (number >= 0) ? number : -number;
 }
 
+//I90494
+module.exports.FizzBuzz = function (number) {
+    if(number%5==0 && number%3==0){
+        return "FizzBuzz";
+    }
+    else if(number%5==0){
+        return "Fizz";
+    }
+    else if(number%3==0){
+        return "Buzz";
+    }
+    else if(number%11==0){
+        return "BuzzBuzz"
+    }
+
+}
+
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -18,3 +18,23 @@ describe('Absolute : 90232 : Muthu Mathiyazhagan K', () => {
         expect(result).toBe(0);
     })
 })
+
+describe("FizzBuzz : I90494 : Atharsh S", () => {
+    it("should return 'FizzBuzz' if input can be divide by 5 and 3", () =>{
+        const result = lib.FizzBuzz(15);
+        expect(result).toBe('FizzBuzz');
+    })
+    it("should return 'Fizz' if input can be divide by 5", () =>{
+        const result = lib.FizzBuzz(5);
+        expect(result).toBe('Fizz');
+    })
+    it("should return 'Buzz' if input can be divide by 3", () =>{
+        const result = lib.FizzBuzz(3);
+        expect(result).toBe('Buzz');
+    })
+    it("should return 'BuzzBuzz' if input can be divide by 11", () =>{
+        const result = lib.FizzBuzz(11);
+        expect(result).toBe('BuzzBuzz');
+    })
+    
+})
